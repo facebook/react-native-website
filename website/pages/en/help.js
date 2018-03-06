b81fd4f5097e212225b7d869618563ebe14dbf60
Hector Ramos <hramos@fb.com>
2018-03-06 13:05:40 -0800

    Link to Discuss

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -17,100 +17,48 @@ class Help extends React.Component {
     return (
       <div className="pageContainer">
         <Container className="mainContainer documentContainer postContainer">
-          <h1>Community Resources</h1>
+          <h1>Where To Get Support</h1>
           <p>
-            At Facebook, there are dozens of engineers who work on React Native
-            full-time. But there are far more people in the community who make
-            key contributions and fix things. So if you need help with your
-            React Native app, the right place to go depends on the type of help
-            that you need.
+            <strong>React Native</strong> is worked on full-time by Facebook's
+            product infrastructure engineering team. But there are far more
+            people in the community who make key contributions and fix things.
+            So if you need help with your React Native app, the right place to
+            go depends on the type of help that you need.
           </p>
-        </Container>
-        <Container className="mainContainer">
-          <h2>Browse the docs</h2>
+          <h2>Stack Overflow</h2>
           <p>
-            Find what you're looking for in our detailed documentation and
-            guides.
-          </p>
-          <ul>
-            <li>
-              <a href={siteConfig.baseUrl + "docs/getting-started.html"}>
-                Getting Started
-              </a>
-            </li>
-            <li>
-              <a href={siteConfig.baseUrl + "docs/tutorial.html"}>
-                Learn the Basics
-              </a>
-            </li>
-            <li>
-              <a href={siteConfig.baseUrl + "docs/components-and-apis.html"}>
-                Components and APIs
-              </a>
-            </li>
-            <li>
-              <a
-                href={
-                  siteConfig.baseUrl +
-                  "docs/integration-with-existing-apps.html"
-                }
-              >
-                Integrating with Existing Apps
-              </a>
-            </li>
-          </ul>
-          <h2>Join the community</h2>
-          <p>
-            Connect with other React Native developers. Show off your project,
-            or ask how other people solved similar problems.
-          </p>
-          <ul>
-            <li>
-              <a href="http://stackoverflow.com/questions/tagged/react-native?sort=frequent">
-                Stack Overflow
-              </a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/groups/react.native.community">
-                React Native Community
-              </a>
-            </li>
-            <li>
-              <a href="https://discord.gg/0ZcbPKXt5bZjGY5n">Reactiflux Chat</a>
-            </li>
-          </ul>
-          <h2>Stay up to date</h2>
-          <p>Find out what's happening in the world of React Native.</p>
-          <ul>
-            <li>
-              <a href="https://twitter.com/reactnative">
-                React Native on Twitter
-              </a>
-            </li>
-            <li>
-              <a href="http://facebook.github.io/react-native/blog/">
-                The React Native Blog
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/facebook/react-native/releases">
-                Release notes
-              </a>
-            </li>
-          </ul>
-        </Container>
-        <Container padding={["bottom"]}>
-          <h2>Contributing to React Native</h2>
-          <p>
-            React Native is{" "}
-            <a href="https://github.com/facebook/react-native/">open source</a>!
-            If you want to contribute, read the{" "}
-            <a href={siteConfig.baseUrl + "docs/contributing.html"}>
-              Contributor's Guide
+            Many members of the community use Stack Overflow to ask questions.
+            Read through the{" "}
+            <a href="http://stackoverflow.com/questions/tagged/react-native?sort=frequent">
+              existing questions
             </a>{" "}
-            and{" "}
-            <a href={siteConfig.baseUrl + "docs/maintainers.html"}>
-              What to Expect from Maintainers
+            tagged with <strong>react-native</strong> or{" "}
+            <a href="http://stackoverflow.com/questions/ask?tags=react-native">
+              ask your own
+            </a>!
+          </p>
+          <h2>Discussion Forum</h2>
+          <p>
+            For longer-form conversations about React Native, we’ve set up a{" "}
+            <a href="https://discuss.reactjs.org/t/welcome-react-native-community-group/10239">
+              discussion forum at <strong>discuss.reactjs.org</strong>
+            </a>. This forum is a great place for discussion about best
+            practices and application architecture as well as the future of
+            React Native. If you have an answerable code-level question, please
+            post it to Stack Overflow instead.
+          </p>
+          <h2>Reactiflux Chat</h2>
+          <p>
+            If you need an answer right away, check out the{" "}
+            <a href="https://discord.gg/0ZcbPKXt5bZjGY5n">Reactiflux Discord</a>{" "}
+            community. There are usually a number of React Native experts there
+            who can help out or point you to somewhere you might want to look.
+          </p>
+          <h2>Twitter</h2>
+          <p>
+            For the latest news about React,{" "}
+            <a href="https://twitter.com/reactnative">
+              follow <strong>@reactnative</strong> on Twitter
             </a>.
           </p>
         </Container>
