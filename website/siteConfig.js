af232dfb4a1dca67ba4a9205aa4768b71053155c
Hector Ramos <hramos@fb.com>
2018-09-12 08:33:51 -0700

    Promote 0.57-RC to stable

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,7 +8,7 @@
 const RemarkablePlugins = require("./core/RemarkablePlugins");
 
 const users = require("./showcase.json");
-const defaultVersionShown = "0.56";
+const defaultVersionShown = "0.57";
 const baseUrl = "/react-native/";
 const repoUrl = "https://github.com/facebook/react-native";
 const siteConfig = {
