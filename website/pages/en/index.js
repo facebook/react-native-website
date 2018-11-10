471fe8b295d567da31c6ce6176d36708c8ca88d5
Yassine Imounachen <yassine.imounachen@gmail.com>
2018-11-11 00:52:11 +0100

    I think that Swift is more relevant for implied iOS development

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -142,7 +142,7 @@ class WhyReactNativeIsSoGreat extends Component {
               <MarkdownBlock>
                 With React Native, you don't build a "mobile web app", an "HTML5
                 app", or a "hybrid app". You build a real mobile app that's
-                indistinguishable from an app built using Objective-C or Java.
+                indistinguishable from an app built using Swift or Java.
                 React Native uses the same fundamental UI building blocks as
                 regular iOS and Android apps. You just put those building blocks
                 together using JavaScript and React.
