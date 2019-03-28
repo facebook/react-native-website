5a209670b4bfcd92a5bf35bf3f9419aaa36a4030
Adrian Carolli <adrian.carolli1@gmail.com>
2019-03-28 16:33:10 -0400

    Select component for upgrade page

--- website/pages/en/upgrade.js
+++ website/pages/en/upgrade.js
@@ -11,6 +11,7 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const Container = CompLibrary.Container;
 
 const siteConfig = require(process.cwd() + '/siteConfig.js');
+const versions = require(process.cwd() + '/versions.json');
 
 class Button extends React.Component {
   render() {
@@ -21,6 +22,23 @@ class Button extends React.Component {
         target={this.props.target}>
         {this.props.children}
       </a>
+    );
+  }
+}
+
+class Select extends React.Component {
+  renderOption(option) {
+    return (
+      <option key={option} value={option}>
+        {option}
+      </option>
+    );
+  }
+  render() {
+    return (
+      <select selected={this.props.selected}>
+        {this.props.options.map(option => this.renderOption(option))}
+      </select>
     );
   }
 }
@@ -39,6 +57,12 @@ class Upgrade extends React.Component {
           </p>
           <h2>Select the options matching your project</h2>
           <h3>React Native Version</h3>
+          <Select options={versions} />
+          to
+          <Select
+            options={versions}
+            selected={siteConfig.defaultVersionShown}
+          />
           <h3>What type of project do you have?</h3>
           <h3>What are some of your app dependencies?</h3>
           <h3>How would you like to update?</h3>
