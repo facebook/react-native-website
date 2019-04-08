56d53c01dcdee3d0ac5641dfaf8369f8985b0cc3
Adrian Carolli <adrian.carolli1@gmail.com>
2019-04-08 15:32:49 -0400

    Refactor - remove stuff we won't need for our first release - replicate what pvinis rn-purge-web does

--- website/pages/en/upgrade.js
+++ website/pages/en/upgrade.js
@@ -6,167 +6,111 @@
  */
 
 const React = require('react');
-const fs = require('fs');
 
 const CompLibrary = require('../../core/CompLibrary.js');
 const Container = CompLibrary.Container;
 
 const siteConfig = require(process.cwd() + '/siteConfig.js');
-const versions = require(process.cwd() + '/versions.json');
 
-const formConfig = {
-  projectTypes: ['React Native'],
-  upgradeTypes: ['React Native CLI', 'Automated', 'Manually'],
-};
+// we must adjust the data to be x.y.z versions because we need this format
+// https://github.com/pvinis/rn-diff-purge/compare/version/0.59.0..version/0.59.0
+const versions = require(process.cwd() + '/versions.json').map(v => v + '.0');
+const defaultVersionShown = siteConfig.defaultVersionShown + '.0';
+const fromVersion = '0.58.0';
+const toVersion = defaultVersionShown;
 
-// TODO: consider moving the below functions somewhere else
-
-const clearVersionFolderName = version =>
-  parseFloat(version.replace('version-0.', ''));
-const clearVersion = version => parseFloat(fromVersion.replace('0.', ''));
-
-const getUpgradeInstructions = ({fromVersion, toVersion}) => {
-  const versions = fs
-    .readdirSync('./versioned_docs')
-    .filter(
-      version =>
-        clearVersionFolderName(version) > clearVersion(fromVersion) &&
-        clearVersionFolderName(version) < clearVersion(toVersion)
-    );
-
-  console.log({versions});
-
-  const instructions = versions.map(version =>
-    require(`${process.cwd()}/versioned_docs/${version}/upgradeInstructions.json`)
-  );
-
-  console.log({instructions});
-};
-
-const Button = ({onClick, children}) => (
-  <a className="big-button" onClick={onClick}>
+const Link = ({href, children, id, target}) => (
+  <a href={href} id={id} target={target}>
     {children}
   </a>
 );
 
-const Select = ({selected, options, onChange}) => {
+const Button = ({onClick, children, id}) => (
+  <a className="big-button" onClick={onClick} id={id}>
+    {children}
+  </a>
+);
+
+const Select = ({selected, options, onChange, id}) => {
   const renderOption = option => (
     <option key={option} value={option}>
       {option}
     </option>
   );
   return (
-    <select value={selected} onChange={onChange}>
+    <select value={selected} onChange={onChange} id={id}>
       {options.map(option => renderOption(option))}
     </select>
   );
 };
 
-const RadioGroup = ({name, values, selected, onChange}) => {
-  const renderRadio = value => (
-    <div key={value}>
-      <label>
-        <input
-          type="radio"
-          name={name}
-          value={value}
-          checked={value === selected}
-          onChange={onChange}
-        />
-        {value}
-      </label>
-    </div>
-  );
-
-  return <div>{values.map(value => renderRadio(value))}</div>;
-};
-
-class Upgrade extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projectType: 'React Native',
-      upgradeType: 'React Native CLI',
-      fromVersion: '0.58',
-      toVersion: siteConfig.defaultVersionShown,
-    };
-  }
-
-  handleShowUpgradeInstructions(event) {
-    event.preventDefault();
-
-    getUpgradeInstructions({
-      fromVersion: this.state.fromVersion,
-      toVersion: this.state.toVersion,
-    });
-
-    // TODO: show in the page
-  }
-
-  render() {
-    const {fromVersion, toVersion, projectType, upgradeType} = this.state;
-
-    return (
-      <div className="pageContainer">
-        <Container className="mainContainer documentContainer postContainer">
-          <h1>Upgrading to new React Native versions</h1>
-          <p>
-            Upgrading to new versions of React Native will give you access to
-            more APIs, views, developer tools and other goodies. Upgrading
-            requires a small amount of effort, but we try to make it easy for
-            you.
-          </p>
-          <h2>Select the options matching your project</h2>
-          <p>
-            Finding the best way to upgrade can be a challenge. So, we built a
-            tool to make it easier that helps you upgrade with ease.
-          </p>
-          <h3>React Native Version</h3>
-          <p>
-            Select which version you are coming from and which version you want
-            to upgrade to. We auto-selected version{' '}
-            {siteConfig.defaultVersionShown} for your convenience.
-          </p>
-          <Select
-            options={versions}
-            selected={fromVersion}
-            onChange={version => this.setState({fromVersion: version})}
-          />
-          to
-          <Select
-            options={versions}
-            selected={toVersion}
-            onChange={version => this.setState({toVersion: version})}
-          />
-          <h3>What type of project do you have?</h3>
-          <p>
-            Select what type of React Native project you're using. Eventually we
-            will support Expo and ejected Expo projects but for now we only
-            allow for React Native ones.
-          </p>
-          <RadioGroup
-            name="projectType"
-            values={formConfig.projectTypes}
-            selected={this.state.projectType}
-            onChange={type => this.setState({upgradeType: type})}
-          />
-          <h3>How would you like to upgrade?</h3>
-          <RadioGroup
-            name="upgradeType"
-            values={formConfig.upgradeTypes}
-            selected={upgradeType}
-            onChange={type => this.setState({upgradeType: type})}
-          />
-          <div className="buttons-unit">
-            <Button onClick={this.handleShowUpgradeInstructions}>
-              Show me how to upgrade!
-            </Button>
-          </div>
-        </Container>
+const Upgrade = () => (
+  <div className="pageContainer">
+    <Container className="mainContainer documentContainer postContainer">
+      <h1>Upgrading to new React Native versions</h1>
+      <p>
+        Upgrading to new versions of React Native will give you access to more
+        APIs, views, developer tools and other goodies. Upgrading requires a
+        small amount of effort, but we try to make it easy for you.
+      </p>
+      <h2>Select the options matching your project</h2>
+      <p>
+        Finding the best way to upgrade can be a challenge. So, we built a tool
+        to make it easier that helps you upgrade with ease.
+      </p>
+      <h3>React Native Version</h3>
+      <p>
+        Select which version you are coming from and which version you want to
+        upgrade to. We auto-selected version {fromVersion} for your convenience.
+      </p>
+      <Select options={versions} selected={fromVersion} id="fromSelect" />
+      to
+      <Select options={versions} selected={toVersion} id="toSelect" />
+      <div className="buttons-unit">
+        <Button id="upgradeButton">Show me how to upgrade!</Button>
       </div>
-    );
-  }
-}
+      <div id="results" className="hide">
+        <Link id="diffLink" target="_blank">
+          Diff here
+        </Link>
+        <br />
+        <Link id="patchLink" target="_blank">
+          Patch here
+        </Link>
+      </div>
+    </Container>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          // TODO: Once V2 Docusaurus lands we shouldn't do this.
+          // see: https://github.com/facebook/Docusaurus/issues/582#issuecomment-382488782
+
+          function showInstructions() {
+            var results = document.getElementById('results');
+            results.style.display = "block";
+
+            var fromSelect = document.getElementById('fromSelect');
+            var fromVersion = fromSelect.options[fromSelect.selectedIndex].value;
+
+            var toSelect = document.getElementById('toSelect');
+            var toVersion = toSelect.options[toSelect.selectedIndex].value;
+
+            var diffLink = "https://github.com/pvinis/rn-diff-purge/compare/version/${fromVersion}..version/${toVersion}";
+            var patchLink = "https://raw.githubusercontent.com/pvinis/rn-diff-purge/master/diffs/${fromVersion}..${toVersion}.diff";
+            
+            var diffLinkElement = document.getElementById('diffLink');
+            diffLinkElement.href = diffLink;
+
+            var patchLinkElement = document.getElementById('patchLink');
+            patchLinkElement.href = patchLink;
+          }
+          var button = document.getElementById('upgradeButton');
+          button.onclick = showInstructions;
+        `,
+      }}
+    />
+  </div>
+);
 
 Upgrade.defaultProps = {
   language: 'en',
