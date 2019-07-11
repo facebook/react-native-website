f91b6dafa1db36794a301b5b6ebed60cd21d9d12
Nat Alison <tesseralis@gmail.com>
2019-07-11 13:37:24 -0700

    adjust animation steps

--- website/static/js/headerAnimation.js
+++ website/static/js/headerAnimation.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const steps = ['full', 'mobile', 'desktop', 'laptop', 'mobile2', 'full2'];
+  const timeouts = [1250, 1500, 1500, 1500, 1500, 1250];
   let i = 0;
   const logo = document.querySelector('.LogoAnimation');
 
-  setInterval(() => {
-    logo.classList.remove('init');
+  function animateStep() {
     const prev = steps[i];
     logo.classList.remove(prev);
     i = (i + 1) % steps.length;
     const current = steps[i];
+    const timeout = timeouts[i];
     logo.classList.add(current);
-  }, 1500);
+
+    setTimeout(animateStep, timeout);
+  }
+
+  setTimeout(() => {
+    logo.classList.remove('init');
+    animateStep();
+  }, 2000);
 });
