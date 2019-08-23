1e971a11ca6de94d86deddbabeb7a0a71019be50
DengYun <tdzl2003@gmail.com>
2019-08-24 02:11:58 +0800

    Update doccode.js

--- cnwebsite/static/js/doccode.js
+++ cnwebsite/static/js/doccode.js
@@ -8,7 +8,7 @@
     }
     var el = document.createElement('div');
     el.innerHTML =
-      '<a href="https://gio.ren/w/noqg4rRk" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">免费领取 &gt;</span></a>';
+      '<a href="https://gio.ren/w/noqg4rRk" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时免费领取。 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">立即查看 &gt;</span></a>';
 
     container.insertBefore(el, container.firstChild);
   }
