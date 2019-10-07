e7c7d1952f78e8237e8555553d0fa10bd72d78a1
sunnylqm <sunnylqm@qq.com>
2019-10-07 22:06:13 +0800

    Update

--- cnwebsite/static/js/doccode.js
+++ cnwebsite/static/js/doccode.js
@@ -2,14 +2,21 @@
   document.addEventListener('DOMContentLoaded', init);
 
   function init() {
-    var container = document.querySelector('.post article');
-    if (!container) {
-      return;
-    }
-    var el = document.createElement('div');
-    el.innerHTML =
-      '<a href="https://gio.ren/w/noqg4rRk" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时免费领取。 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">立即查看 &gt;</span></a>';
+    var articleContainer = document.querySelector('.post article');
+    if (articleContainer) {
+      var el = document.createElement('div');
+      el.innerHTML =
+        '<a href="https://gio.ren/w/noqg4rRk" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时免费领取。 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">立即查看 &gt;</span></a>';
 
-    container.insertBefore(el, container.firstChild);
+      articleContainer.insertBefore(el, articleContainer.firstChild);
+    }
+
+    var sideNav = document.querySelector('#docsNav nav');
+    if (sideNav) {
+      var el = document.createElement('div');
+      el.innerHTML =
+        '<a href="https://activity.huaweicloud.com/appdevelopment.html?&fromuser=aHc4NzgyMjgwMQ==&utm_source=aHc4NzgyMjgwMQ==&utm_medium=cps&utm_campaign=201905" target="_blank" style="display:block;padding:15px 25px 0 0"><img src="/img/thirdparty/huawei.jpg"></a>';
+      sideNav.appendChild(el);
+    }
   }
 })();
