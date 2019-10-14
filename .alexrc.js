a2663b17defb80d9d4455dd22f1fa32a2dfbdebf
Rick Hanlon <rickhanlonii@fb.com>
2019-10-14 17:26:45 +0100

    Update alex rules

--- .alexrc.js
+++ .alexrc.js
@@ -6,25 +6,8 @@
  */
 
 exports.allow = [
-  // We frequently refer to the color property for styling.
-  // However, we're keeping "colored" as disallowed and will use alternatives.
-  "color",
-  "colors",
-
-  // We frequently refer to executing scripts.
-  "executed",
-  "execution",
-
-  // We frequently refer to failures in errors but never in people.
-  "failure",
-  "failures",
-
-  // We frequently refer to events firing,
-  // and it's unclear what context this is profane.
-  "fire",
-  "fires",
-
   // We frequently refer to form props by their name "disabled".
+  // Ideally we would alex-ignore only the valid uses (PRs accepted).
   "invalid",
 
   // Unfortunately "watchman" is a library name that we depend on.
