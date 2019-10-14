4682e6e11992ff76c741733a52443e4a758fe9cd
Rick Hanlon <rickhanlonii@fb.com>
2019-10-14 17:15:54 +0100

    Minor updates to formatting

--- .alexrc.js
+++ .alexrc.js
@@ -6,27 +6,30 @@
  */
 
 exports.allow = [
-  // We frequently refer to the color property for styling
+  // We frequently refer to the color property for styling.
+  // However, we're keeping "colored" as disallowed and will use alternatives.
   "color",
   "colors",
 
-  // We frequently refer to events firing, and it's unclear what context this is profane.
-  "fire",
-  "fires",
-
-  // We frequently refer executing scripts
+  // We frequently refer to executing scripts.
   "executed",
   "execution",
 
-  // We frequently refer to failures in errors but never in people
+  // We frequently refer to failures in errors but never in people.
   "failure",
   "failures",
 
-  // We frequently refer to form props by their name "disabled"
+  // We frequently refer to events firing,
+  // and it's unclear what context this is profane.
+  "fire",
+  "fires",
+
+  // We frequently refer to form props by their name "disabled".
   "invalid",
 
-  // Unfortunately this is a library name we depend on
+  // Unfortunately "watchman" is a library name that we depend on.
   "watchman-watchwoman"
 ];
 
+// Use a "maybe" level of profanity instead of the default "unlikely".
 exports.profanitySureness = 1;
