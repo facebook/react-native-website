35ab72c989f0ad9f004c187cc2769776e69cc55f
sunnylqm <sunnylqm@qq.com>
2020-03-25 11:35:21 +0800

    update doccode

--- cnwebsite/static/js/doccode.js
+++ cnwebsite/static/js/doccode.js
@@ -10,14 +10,14 @@
   }
 
   function init() {
-    // var articleContainer = document.querySelector('.post article');
-    // if (articleContainer) {
-    //   var el = document.createElement('div');
-    //   el.innerHTML =
-    //     '<a href="https://gio.ren/w/noqg4rRk" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时免费领取。 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">立即查看 &gt;</span></a>';
+    var articleContainer = document.querySelector('.post article');
+    if (articleContainer) {
+      var el = document.createElement('div');
+      el.innerHTML =
+        '<a href="https://datayi.cn/w/xogjV6OP" target="_blank" style="display:block;padding:12px;background-color: #eee;color:#666;"><span style="font-weight: bold;color: #05a5d1;">React 实战教程</span> 深入学习一线大厂必备前端技能，VIP 教程限时免费领取。 <span style="border:solid 1px #666; padding-left:4px; padding-right: 4px;padding-top:2px;padding-bottom:2px;margin-left: 8px;vertical-align:middle;">立即查看 &gt;</span></a>';
 
-    //   articleContainer.insertBefore(el, articleContainer.firstChild);
-    // }
+      articleContainer.insertBefore(el, articleContainer.firstChild);
+    }
 
     if (location.href.indexOf('/getting-started') !== -1) {
       var onPageNav = document.querySelector('nav.onPageNav');
