d7ae6d42029a8d54a4b68d84d2033b59ef2fe81f
Bartosz Kaszubowski <gosimek@gmail.com>
2020-07-01 13:35:55 +0200

    slide up announcement banner on scroll

--- website/static/js/announcement.js
+++ website/static/js/announcement.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.fixedHeaderContainer');
   if (container) {
     const announcement = document.createElement('div');
@@ -8,3 +8,45 @@ document.addEventListener('DOMContentLoaded', function() {
     container.insertBefore(announcement, container.firstChild);
   }
 });
+
+const scrollStartBoundary = 160;
+
+window.onscroll = () => {
+  const fixedHeaderContainer = document.querySelector('.fixedHeaderContainer');
+  const navPusher = document.querySelector('.navPusher');
+
+  if (
+    document.body.scrollTop > scrollStartBoundary ||
+    document.documentElement.scrollTop > scrollStartBoundary
+  ) {
+    fixedHeaderContainer.style.top = '-100px';
+    navPusher.style.top = '-100px';
+    navPusher.style.marginBottom = '-100px';
+
+    // Desktop layout
+    if (!document.querySelector('.navBreadcrumb').offsetParent) {
+      document.querySelector('.docsNavContainer').style.top = '60px';
+      document.querySelector('.onPageNav').style.top = '72px';
+      document.querySelector('.docsContainer').style.paddingTop = '100px';
+    }
+  } else {
+    fixedHeaderContainer.style.top = '0';
+    navPusher.style.top = '0';
+    navPusher.style.marginBottom = '0';
+
+    // Desktop layout
+    if (!document.querySelector('.navBreadcrumb').offsetParent) {
+      document.querySelector('.docsNavContainer').style.top = '136px';
+      document.querySelector('.onPageNav').style.top = '172px';
+      document.querySelector('.docsContainer').style.paddingTop = '0';
+    }
+  }
+};
+
+window.onresize = () => {
+  // Fix desktop resizing
+  if (document.querySelector('.navBreadcrumb').offsetParent) {
+    document.querySelector('.docsNavContainer').style.top = undefined;
+    document.querySelector('.docsContainer').style.paddingTop = undefined;
+  }
+};
