2c3e083a3bb2240cb7a9e3ddc4758e796e85fb95
Darsh <darshkpatel@gmail.com>
2020-08-19 11:18:25 +0530

    upgrade to v2 config

---
+++ website/docusaurus.config.js
@@ -0,0 +1,528 @@
+module.exports = {
+  title: 'React Native',
+  tagline: 'A framework for building native apps using React',
+  url: 'https://reactnative.dev',
+  baseUrl: '/',
+  projectName: 'react-native',
+  scripts: [
+    'https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js',
+    'https://snack.expo.io/embed.js',
+    'https://platform.twitter.com/widgets.js',
+    'https://buttons.github.io/buttons.js',
+    '/js/codeblocks.js',
+    '/js/tabs.js',
+    '/js/docs-rating.js',
+    '/js/announcement.js',
+  ],
+  favicon: 'img/favicon.ico',
+  customFields: {
+    repoUrl: 'https://github.com/facebook/react-native',
+    defaultVersionShown: '0.63',
+    users: [
+      {
+        name: 'Facebook',
+        icon: 'facebook.png',
+        linkAppStore: 'https://itunes.apple.com/app/facebook/id284882215',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.facebook.katana',
+        infoLink:
+          'https://code.facebook.com/posts/895897210527114/dive-into-react-native-performance/',
+        infoTitle: 'Using React Native in the Facebook App',
+        pinned: true,
+      },
+      {
+        name: 'Facebook Ads Manager',
+        icon: 'adsmanager.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/facebook-ads-manager/id964397083?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.facebook.adsmanager',
+        infoLink:
+          'https://code.facebook.com/posts/1189117404435352/react-native-for-android-how-we-built-the-first-cross-platform-react-native-app/',
+        infoTitle: 'How We Built the First Cross-Platform React Native App',
+        pinned: true,
+      },
+      {
+        name: 'Facebook Analytics',
+        icon: 'fba.png',
+        linkAppStore: 'https://itunes.apple.com/us/app/facebook/id1266461465',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.facebook.analytics',
+        infoLink: 'https://analytics.facebook.com/blog',
+        infoTitle: '',
+        pinned: true,
+      },
+      {
+        name: 'Instagram',
+        icon: 'instagram.png',
+        linkAppStore:
+          'https://itunes.apple.com/app/instagram/id389801252?pt=428156&ct=igweb.unifiedHome.badge&mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DunifiedHome%26utm_medium%3Dbadge',
+        infoLink:
+          'https://engineering.instagram.com/react-native-at-instagram-dd828a9a90c7#.3h4wir4zr',
+        infoTitle: 'React Native at Instagram',
+        pinned: true,
+      },
+      {
+        name: 'Oculus',
+        icon: 'oculus.png',
+        linkAppStore: 'https://apps.apple.com/us/app/oculus/id1366478176',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.oculus.twilight&hl=en_US',
+        infoLink: '',
+        infoTitle: '',
+        pinned: true,
+      },
+      {
+        name: 'Skype',
+        icon: 'skype.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/skype-for-iphone/id304878510?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.skype.raider',
+        infoLink: 'https://dev.skype.com/react',
+        infoTitle: '',
+        pinned: true,
+      },
+      {
+        name: 'Discord',
+        icon: 'discord.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/discord-chat-for-gamers/id985746746?mt=8',
+        infoLink:
+          'https://blog.discord.com/how-discord-achieves-native-ios-performance-with-react-native-390c84dcd502',
+        infoTitle:
+          'How Discord achieves native iOS performance with React Native',
+        pinned: true,
+      },
+      {
+        name: 'Bloomberg',
+        icon: 'bloomberg.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/bloomberg/id281941097?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.bloomberg.android.plus',
+        infoLink:
+          'https://www.techatbloomberg.com/blog/bloomberg-used-react-native-develop-new-consumer-app/',
+        infoTitle:
+          'How Bloomberg Used React Native to Develop its new Consumer App',
+        pinned: true,
+      },
+      {
+        name: 'Pinterest',
+        icon: 'pinterest.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/pinterest/id429047995?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.pinterest',
+        infoLink:
+          'https://medium.com/@Pinterest_Engineering/supporting-react-native-at-pinterest-f8c2233f90e6',
+        infoTitle: 'Supporting React Native at Pinterest',
+        pinned: true,
+      },
+      {
+        name: 'Tesla',
+        icon: 'tesla.png',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.teslamotors.tesla',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/tesla-motors/id582007913?mt=8',
+        infoLink: 'https://www.tesla.com/blog',
+        infoTitle: '',
+        pinned: true,
+      },
+      {
+        name: 'Uber Eats',
+        icon: 'ubereats.png',
+        infoLink: 'https://eng.uber.com/ubereats-react-native/',
+        infoTitle: 'Powering Uber Eats with React Native',
+        pinned: true,
+      },
+      {
+        name: 'Walmart',
+        icon: 'walmart.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/walmart-app-shopping-savings/id338137227?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.walmart.android',
+        infoLink:
+          ' https://medium.com/walmartlabs/react-native-at-walmartlabs-cdd140589560#.ueonqqloc',
+        infoTitle: 'React Native at Walmart Labs',
+        pinned: true,
+      },
+      {
+        name: 'Wix.com',
+        icon: 'wix.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/wix-com/id1099748482?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.wix.android',
+        infoLink: 'https://www.wix.com/wix-lp/wix-app',
+        infoTitle: 'Get the New Wix App!',
+        pinned: true,
+      },
+      {
+        name: 'Salesforce',
+        icon: 'salesforce.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/salesforce/id404249815?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.salesforce.chatter',
+        infoLink:
+          'https://www.salesforce.com/products/einstein/einstein-voice/',
+        infoTitle: 'Einstein Voice Assistant: Voice Activated AI',
+        pinned: true,
+      },
+      {
+        name: 'Artsy',
+        icon:
+          'https://raw.githubusercontent.com/artsy/eigen/master/Artsy/Resources/Images.xcassets/AppIcon.appiconset/AppIcon167.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/artsy-collect-bid-on-fine/id703796080?mt=8',
+        infoLink: 'https://artsy.github.io/series/react-native-at-artsy/',
+        infoTitle: 'React Native at Artsy',
+        pinned: true,
+      },
+      {
+        name: 'Baidu Mobile (手机百度)',
+        icon: 'baidu.png',
+        linkPlayStore: 'http://shouji.baidu.com/software/9896302.html',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/%E6%89%8B%E6%9C%BA%E7%99%BE%E5%BA%A6-%E7%99%BE%E5%BA%A6%E4%B8%80%E4%B8%8B%E4%BD%A0%E5%B0%B1%E5%BE%97%E5%88%B0/id382201985?mt=8',
+        infoLink:
+          'http://baike.baidu.com/link?url=TW8YhcVN4tO_Jz5VqMclCjGhf12EEqMD_TeVC6efe2REZlx80r6T0dX96hdmNl36XogLyExXzrvFU9rFeqxg_K',
+        infoTitle:
+          'Baidu Mobile is a search engine used by over 600 million people in China',
+        pinned: false,
+      },
+      {
+        name: 'CBS Sports Franchise Football',
+        icon: 'cbssports.png',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.cbssports.fantasy.franchisefootball2015',
+        infoLink:
+          'http://www.cbssports.com/fantasy/football/games/franchise/2015',
+        infoTitle:
+          'Award winning Fantasy Football league manager built with React Native',
+        pinned: false,
+      },
+      {
+        name: 'Chop',
+        icon: 'chop.png',
+        linkAppStore: 'http://apple.co/2dfkYH9',
+        infoLink: 'http://blog.getchop.io/2016/10/13/how-we-built-chop/',
+        infoTitle: 'How we built Chop using React Native',
+        pinned: false,
+      },
+      {
+        name: 'Delivery.com',
+        icon: 'delivery.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/delivery.com-food-alcohol/id435168129?mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.deliverycom',
+        infoLink:
+          'https://medium.com/delivery-com-engineering/react-native-in-an-existing-ios-app-delivered-874ba95a3c52#.37qruw6ck',
+        infoTitle: 'React Native in an Existing iOS App: Getting Started',
+        pinned: false,
+      },
+      {
+        name: 'F8',
+        icon: 'f8.png',
+        linkAppStore:
+          'https://itunes.apple.com/it/app/f8/id853467066?l=en&mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.facebook.f8',
+        infoLink: 'http://makeitopen.com/docs/en/1-1-planning.html',
+        infoTitle: 'Tutorial: Building the F8 conference app',
+        pinned: false,
+      },
+      {
+        name: 'Gyroscope',
+        icon: 'gyroscope.png',
+        linkAppStore:
+          'https://itunes.apple.com/app/apple-store/id1104085053?pt=117927205&ct=website&mt=8',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.gyroscope.gyroscope',
+        infoLink: 'https://blog.gyrosco.pe/building-the-app-1dac1a97d253',
+        infoTitle: 'Building a visualization experience with React Native',
+        pinned: false,
+      },
+      {
+        name: 'Huiseoul (惠首尔)',
+        icon: 'huiseoul.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/hui-shou-er-ni-si-ren-mei/id1127150360?ls=1&mt=8',
+        infoLink:
+          'https://engineering.huiseoul.com/building-a-conversational-e-commerce-app-in-6-weeks-with-react-native-c35d46637e07',
+        infoTitle:
+          'Building a conversational E-commerce app in 6 weeks with React Native',
+        pinned: false,
+      },
+      {
+        name: 'JD（手机京东）',
+        icon: 'jdcom.png',
+        linkAppStore:
+          'https://itunes.apple.com/cn/app/shou-ji-jing-dong-xin-ren/id414245413?mt=8',
+        linkPlayStore: 'https://app.jd.com/android.html',
+        infoLink: 'http://ir.jd.com/phoenix.zhtml?c=253315&p=irol-homeProfile',
+        infoTitle:
+          'JD.com is China’s largest ecommerce company by revenue and a member of the Fortune Global 500.',
+        pinned: false,
+      },
+      {
+        name: 'SoundCloud Pulse',
+        icon: 'soundcloud_pulse.jpg',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/soundcloud-pulse-for-creators/id1074278256?mt=8',
+        infoLink:
+          'https://developers.soundcloud.com/blog/react-native-at-soundcloud',
+        infoTitle: 'Why React Native worked well for us',
+        pinned: false,
+      },
+      {
+        name: 'Tencent QQ',
+        icon: 'qq.png',
+        linkPlayStore:
+          'http://android.myapp.com/myapp/detail.htm?apkName=com.tencent.mobileqq',
+        infoLink: 'https://www.tencent.com/en-us/system.html',
+        infoTitle:
+          "QQ is China's largest messaging platform, with over 829 million active accounts",
+        pinned: true,
+      },
+      {
+        name: 'Townske',
+        icon: 'townske.png',
+        linkAppStore:
+          'https://itunes.apple.com/us/app/townske-stunning-city-guides/id1018136179?ls=1&mt=8',
+        infoLink:
+          'https://hackernoon.com/townske-app-in-react-native-6ad557de7a7c',
+        infoTitle:
+          'The experience of a web developer building an app using React Native',
+        pinned: false,
+      },
+      {
+        name: 'Vogue',
+        icon: 'vogue.jpeg',
+        linkAppStore:
+          'https://itunes.apple.com/app/apple-store/id1087973225?pt=45076&ct=site-promo&mt=8',
+        infoLink: 'http://www.vogue.com/app',
+        infoTitle: '',
+        pinned: false,
+      },
+      {
+        name: 'adidas GLITCH',
+        icon: 'glitch.png',
+        linkAppStore:
+          'https://itunes.apple.com/gb/app/adidas-glitch-interchangeable/id1166181522?mt=8&cm_sp=FOOTBALL-_-UNDEFINED-_-CTA-DOWNLOAD-IPHONE',
+        linkPlayStore:
+          'https://play.google.com/store/apps/details?id=com.adidas.glitch&cm_sp=FOOTBALL-_-UNDEFINED-_-CTA-DOWNLOAD-ANDROID',
+        infoLink:
+          'https://medium.com/possible-cee/how-we-have-been-breaking-patterns-with-the-adidas-glitch-d734340fd40e',
+        infoTitle: 'Building Adidas GLITCH app with React Native',
+        pinned: false,
+      },
+    ],
+    communityRepos: [],
+    blogSidebarCount: 'ALL',
+    facebookAppId: '1677033832619985',
+    markdownPlugins: [null, null],
+  },
+  onBrokenLinks: 'log',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          homePageId: 'getting-started',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          editUrl:
+            'https://github.com/facebook/react-native-website/blob/master/docs/',
+          path: 'docs',
+          sidebarPath: require.resolve('./sidebars.json'),
+        },
+        blog: {
+          path: 'blog',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/customTheme.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [],
+  themeConfig: {
+    announcementBar: {
+      id: 'blm_banner', // Any value that will identify this message.
+      content:
+        'Black Lives Matter. <a href="https://support.eji.org/give/153413/#!/donation/checkout">Support the Equal Justice Initiative.</a>',
+      backgroundColor: '#fafbfc', // Defaults to `#fff`.
+    },
+    navbar: {
+      title: 'React Native',
+      logo: {
+        src: 'img/header_logo.svg',
+      },
+      items: [
+        {
+          type: 'docsVersion',
+          position: 'left',
+          to: '/versions',
+        },
+        {
+          to: 'docs/',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: 'docs/components-and-apis',
+          label: 'Components',
+          position: 'left',
+        },
+        {
+          to: 'docs/accessibilityinfo',
+          label: 'API',
+          position: 'left',
+        },
+        {
+          to: '/help',
+          label: 'Community',
+          position: 'left',
+        },
+        {
+          to: '/blog',
+          label: 'Blog',
+          position: 'left',
+        },
+
+        {
+          label: 'Version',
+          to: 'docs',
+          position: 'right',
+          items: [
+            {
+              label: '0.63',
+              to: 'docs/',
+              activeBaseRegex: 'docs/(?!0.60|0.61|0.62|0.63|next)',
+            },
+            {
+              label: '0.62',
+              to: 'docs/0.62/',
+            },
+            {
+              label: '0.61',
+              to: 'docs/0.61/',
+            },
+            {
+              label: '0.60',
+              to: 'docs/0.60/',
+            },
+            {
+              label: 'Master/Unreleased',
+              to: 'docs/next/',
+              activeBaseRegex: 'docs/next/(?!support|team|resources)',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/facebook/react-native',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    image: 'img/logo-og.png',
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'DOCS',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/getting-started',
+            },
+            {
+              label: 'Tutorial',
+              to: 'docs/tutorial',
+            },
+            {
+              label: 'Components and APIs',
+              to: 'docs/components-and-apis',
+            },
+            {
+              label: 'More Resources',
+              to: 'docs/more-resources',
+            },
+          ],
+        },
+        {
+          title: 'COMMUNITY',
+          items: [
+            {
+              label: 'The React Native Community',
+              href: 'https://reactnative.dev/help',
+            },
+            {
+              label: "Who's using React Native?",
+              to: 'https://reactnative.dev/showcase',
+            },
+            {
+              label: 'Ask Questions on Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/react-native',
+            },
+            {
+              label: 'Contributor Guide',
+              to:
+                'https://github.com/facebook/react-native/blob/master/CONTRIBUTING.md',
+            },
+            {
+              label: 'DEV Community',
+              to: 'https://dev.to/t/reactnative',
+            },
+          ],
+        },
+        {
+          title: 'MORE RESOURCES',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/reactnative',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/react-native',
+            },
+            {
+              label: 'React',
+              href: 'https://reactjs.org/',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Facebook Open Source Logo',
+        src: 'img/oss_logo.png',
+        href: 'https://opensource.facebook.com',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
+    },
+    algolia: {
+      apiKey: '2c98749b4a1e588efec53b2acec13025',
+      indexName: 'react-native-versions',
+      algoliaOptions: {
+        facetFilters: ['tags:VERSION'],
+        hitsPerPage: 5,
+      },
+    },
+    gtag: {
+      trackingID: 'UA-41298772-2',
+    },
+  },
+};
