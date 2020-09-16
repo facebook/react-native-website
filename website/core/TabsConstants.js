0cf4c808d215b7c1a662277617a79482719f40c0
Bartosz Kaszubowski <gosimek@gmail.com>
2020-09-16 19:57:32 +0200

    refs #20, initial tabs setup on Environment page, tabs constants refactor

--- website/core/TabsConstants.js
+++ website/core/TabsConstants.js
@@ -1,11 +1,46 @@
-const components = [
+const isMacOS = navigator.platform.startsWith('Mac');
+const isWindows = navigator.platform.startsWith('Win');
+
+const syntax = [
   {label: 'Function Component', value: 'functional'},
   {label: 'Class Component', value: 'classical'},
 ];
+const defaultSyntax = 'functional';
 
 const packageManagers = [
   {label: 'npm', value: 'npm'},
   {label: 'Yarn', value: 'yarn'},
 ];
+const defaultPackageManager = 'npm';
 
-export default {components, packageManagers};
+const guides = [
+  {label: 'Expo CLI Quickstart', value: 'quickstart'},
+  {label: 'React Native CLI Quickstart', value: 'native'},
+];
+const defaultGuide = 'quickstart';
+
+const platforms = [
+  {label: 'Android', value: 'android'},
+  {label: 'iOS', value: 'ios'},
+];
+const defaultPlatform = isMacOS ? 'ios' : 'android';
+
+const oses = [
+  {label: 'macOS', value: 'macos'},
+  {label: 'Windows', value: 'windows'},
+  {label: 'Linux', value: 'linux'},
+];
+const defaultOs = isMacOS ? 'macos' : isWindows ? 'windows' : 'linux';
+
+export default {
+  defaultGuide,
+  defaultOs,
+  defaultPackageManager,
+  defaultPlatform,
+  defaultSyntax,
+  guides,
+  oses,
+  packageManagers,
+  platforms,
+  syntax,
+};
