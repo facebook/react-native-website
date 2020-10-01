f11a27a2842542ed0579fed7bc9462f0336a9d05
ronniee123 <72191587+ronniee123@users.noreply.github.com>
2020-10-01 13:59:18 +0530

    Update .alexrc.js

--- .alexrc.js
+++ .alexrc.js
@@ -8,7 +8,7 @@
 exports.allow = [
   // We frequently refer to form props by their name "disabled".
   // Ideally we would alex-ignore only the valid uses (PRs accepted).
-  "invalid",
+  "invalid???",
 
   // Unfortunately "watchman" is a library name that we depend on.
   "watchman-watchwoman",
