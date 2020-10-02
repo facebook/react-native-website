b7915fbb1ecaa81db233be4d870e2355f828ed39
Luis Moreira <51124146+luis-moreira-sonarsource@users.noreply.github.com>
2020-10-02 13:01:29 +0200

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree. adding smt
  */
 
 const RemarkablePlugins = require('./core/RemarkablePlugins');
