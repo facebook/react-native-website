7429eed8f6b435494f1c86ee6e6da949db65b240
Bartosz Kaszubowski <gosimek@gmail.com>
2021-03-08 12:50:08 +0100

    setup and enable PWA (#2420)
    * setup and enable PWA app, update Docusaurus
    
    * revert style tweak after re-swizzle
    
    * update Yarn lock after master merge
    
    * revert leftover changes in DocItem
    
    * revert Prism theme cleanup
    
    * update lock after rebase

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,7 +60,65 @@ module.exports = {
       },
     ],
   ],
-  plugins: ['docusaurus-plugin-sass', './sitePlugin'],
+  plugins: [
+    'docusaurus-plugin-sass',
+    './sitePlugin',
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: ['appInstalled', 'queryString'],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/pwa/manifest-icon-512.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json',
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: '#20232a',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#20232a',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/pwa/manifest-icon-512.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/pwa/manifest-icon-512.png',
+            color: '#06bcee',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            href: '/img/pwa/manifest-icon-512.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#20232a',
+          },
+        ],
+      },
+    ],
+  ],
   themeConfig: {
     prism: {
       defaultLanguage: 'jsx',
