0f951501946f4449b1aea6a521a5892ba5f9f64f
Bartosz Kaszubowski <gosimek@gmail.com>
2021-03-13 12:05:53 +0100

    rewrite redirect for Netlify, cleanup deployment scrips, correct post date

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -296,11 +296,12 @@ module.exports = {
         property: 'og:image',
         content: 'https://reactnative.dev/img/logo-og.png',
       },
-      {name: 'twitter:card', content: 'summary'},
+      {name: 'twitter:card', content: 'summary_large_image'},
       {
         name: 'twitter:image',
         content: 'https://reactnative.dev/img/logo-og.png',
       },
+      {name: 'twitter:site', content: '@reactnative'},
     ],
   },
 };
