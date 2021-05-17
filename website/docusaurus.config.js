8f273c20494a982f816d9b6cdd6e1ca6d885c5f4
R Nabors <rachelnabors@users.noreply.github.com>
2021-05-17 14:33:03 +0100

    Adding survey link for 1% of visitors (#2616)
    * Adding survey link for 1% of visitors
    
    * attempt to fix survey
    
    Co-authored-by: Test User <gosimek@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,6 +15,11 @@ module.exports = {
         'https://cdn.jsdelivr.net/npm/focus-visible@5.2.0/dist/focus-visible.min.js',
       defer: true,
     },
+    {
+      src:
+        'https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8ryO5qrZo8Exadq9qmt1wtm4_2FdZGEAKHDFEt_2BBlwwM4.js',
+      defer: true,
+    },
     {src: 'https://snack.expo.io/embed.js', defer: true},
   ],
   favicon: 'img/favicon.ico',
