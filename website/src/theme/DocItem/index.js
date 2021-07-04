554ea5b7d37a9246e769b1c896bf1168304837ed
Bartosz Kaszubowski <gosimek@gmail.com>
2021-07-04 09:51:20 +0200

    reswizzle `DocItem` to fix version banner issue correctly (#2668)
    * reswizzle `DocItem` to fix version banner issue correctly
    
    * remove swizzled `DocVersionSuggestions` file
    
    * add back `DocsRating` after reswizzle
    
    * list files
    
    * small style fix for the versions page
    
    * build fix
    
    * add missing className after swizzle, small table style fix

--- website/src/theme/DocItem/index.js
+++ website/src/theme/DocItem/index.js
@@ -6,32 +6,31 @@
  */
 import React from 'react';
 import DocPaginator from '@theme/DocPaginator';
-import DocVersionSuggestions from '@theme/DocVersionSuggestions';
+import DocVersionBanner from '@theme/DocVersionBanner';
 import Seo from '@theme/Seo';
 import LastUpdated from '@theme/LastUpdated';
 import TOC from '@theme/TOC';
 import EditThisPage from '@theme/EditThisPage';
+import {MainHeading} from '@theme/Heading';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import {
   useActivePlugin,
-  useVersions,
   useActiveVersion,
+  useVersions,
 } from '@theme/hooks/useDocs';
 
 import DocsRating from '../../../core/DocsRating';
 
 function DocItem(props) {
-  const {content: DocContent} = props;
+  const {content: DocContent, versionMetadata} = props;
+  const {metadata, frontMatter} = DocContent;
   const {
-    metadata,
-    frontMatter: {
-      image,
-      keywords,
-      hide_title: hideTitle,
-      hide_table_of_contents: hideTableOfContents,
-    },
-  } = DocContent;
+    image,
+    keywords,
+    hide_title: hideTitle,
+    hide_table_of_contents: hideTableOfContents,
+  } = frontMatter;
   const {
     description,
     title,
@@ -41,12 +40,15 @@ function DocItem(props) {
     lastUpdatedBy,
     unversionedId,
   } = metadata;
-  const {pluginId} = useActivePlugin({failfast: true});
-
+  const {pluginId} = useActivePlugin({
+    failfast: true,
+  });
   const versions = useVersions(pluginId);
-  const version = useActiveVersion(pluginId);
+  const activeVersion = useActiveVersion(pluginId);
+  const showVersionBadge = versions.length > 1 && !activeVersion.isLast;
 
-  const showVersionBadge = versions.length > 1 && !version.isLast;
+  const shouldAddTitle =
+    !hideTitle && typeof DocContent.contentTitle === 'undefined';
   return (
     <>
       <Seo
@@ -62,44 +64,42 @@ function DocItem(props) {
           className={clsx('col', {
             [styles.docItemCol]: !hideTableOfContents,
           })}>
-          <DocVersionSuggestions />
+          <DocVersionBanner versionMetadata={versionMetadata} />
           <div className={styles.docItemContainer}>
             <article>
               {showVersionBadge && (
-                <div>
-                  <span className="badge badge--secondary">
-                    Version: {version.label}
-                  </span>
-                </div>
-              )}
-              {!hideTitle && (
-                <header>
-                  <h1 className={styles.docTitle}>{title}</h1>
-                </header>
+                <span className="badge badge--secondary">
+                  Version: {versionMetadata.label}
+                </span>
               )}
               <div className="markdown">
+                {/*
+                Title can be declared inside md content or declared through frontmatter and added manually
+                To make both cases consistent, the added title is added under the same div.markdown block
+                See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
+                */}
+                {shouldAddTitle && <MainHeading>{title}</MainHeading>}
                 <DocContent />
               </div>
+              <DocsRating label={unversionedId} />
+              {(editUrl || lastUpdatedAt || lastUpdatedBy) && (
+                <footer className="docMetadata row docusaurus-mt-lg">
+                  <div className="col">
+                    {editUrl && <EditThisPage editUrl={editUrl} />}
+                  </div>
+                  <div className={clsx('col', styles.lastUpdated)}>
+                    {(lastUpdatedAt || lastUpdatedBy) && (
+                      <LastUpdated
+                        lastUpdatedAt={lastUpdatedAt}
+                        formattedLastUpdatedAt={formattedLastUpdatedAt}
+                        lastUpdatedBy={lastUpdatedBy}
+                      />
+                    )}
+                  </div>
+                </footer>
+              )}
             </article>
-            <DocsRating label={unversionedId} />
-            {(editUrl || lastUpdatedAt || lastUpdatedBy) && (
-              <div className="docMetadata margin-vert--xl row">
-                <div className="col">
-                  {editUrl && <EditThisPage editUrl={editUrl} />}
-                </div>
-                <div className="col lastUpdated">
-                  {lastUpdatedAt && (
-                    <LastUpdated
-                      lastUpdatedAt={lastUpdatedAt}
-                      formattedLastUpdatedAt={formattedLastUpdatedAt}
-                    />
-                  )}
-                </div>
-              </div>
-            )}
-            <div className="margin-vert--lg">
-              <DocPaginator metadata={metadata} />
-            </div>
+            <DocPaginator metadata={metadata} />
           </div>
         </div>
         {!hideTableOfContents && DocContent.toc && (
