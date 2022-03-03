bf14ab7fa5286c71a52978f502d176ed62c8ccd3
dmitryvinn <dmitryvinn@gmail.com>
2022-03-02 19:39:33 -0800

    Fixing new line issue that might fail on old browsers

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -155,8 +155,7 @@ const lastVersion = versions[0];
       announcementBar: {
         id: 'support_ukraine',
         content:
-          'Support Ukraine 🇺🇦 <a target="_blank" rel="noopener noreferrer" \
-          href="https://opensource.facebook.com/support-ukraine"> Help Provide Humanitarian Aid to Ukraine</a>.',
+          'Support Ukraine 🇺🇦 <a target="_blank" rel="noopener noreferrer" href="https://opensource.facebook.com/support-ukraine"> Help Provide Humanitarian Aid to Ukraine</a>.',
         backgroundColor: '#20232a',
         textColor: '#fff',
         isCloseable: false,
