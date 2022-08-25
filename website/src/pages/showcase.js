133f5faa8a4097b647806297030200a07cf4a182
Bartosz Kaszubowski <gosimek@gmail.com>
2022-08-25 11:09:03 +0200

    Showcase page refresh (#3235)
    Co-authored-by: Lorenzo Sciandra <lorenzo.sciandra@gmail.com>

--- website/src/pages/showcase.js
+++ website/src/pages/showcase.js
@@ -5,46 +5,49 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import Layout from '@theme/Layout';
-
+import React, {useEffect, useState} from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import ThemedImage from '@theme/ThemedImage';
+
+import {Section} from './index';
+import IconExternalLink from '../theme/Icon/ExternalLink';
 
 const renderApp = (app, i) => {
+  const imgSource = !app.icon.startsWith('http')
+    ? useBaseUrl('img/showcase/' + app.icon)
+    : app.icon;
+
   return (
-    <div className="showcase" key={i}>
-      <div>
-        <a href={app.infoLink}>{renderAppIcon(app)}</a>
-        <h3>{app.name}</h3>
-        {renderLinks(app)}
-        {renderInfo(app.infoTitle, app.infoLink)}
+    <div className="showcase" key={`app-${app.name}-${i}`}>
+      <div className="iconBox">
+        <img src={imgSource} alt={app.name} className="iconBackground" />
+        <img src={imgSource} alt={app.name} className="icon" />
+      </div>
+      <div className="showcaseContent">
+        <div>
+          <h3>{app.name}</h3>
+          {renderLinks(app)}
+        </div>
+        {app.infoLink && (
+          <a
+            className="articleButton"
+            href={app.infoLink}
+            target="_blank"
+            title={app.infoTitle}>
+            Learn more{' '}
+            <IconExternalLink width={12} height={12} style={{opacity: 0.5}} />
+          </a>
+        )}
       </div>
     </div>
   );
 };
 
-const renderAppIcon = app => {
-  let imgSource = app.icon;
-  if (!app.icon.startsWith('http')) {
-    imgSource = useBaseUrl('img/showcase/' + app.icon);
-  }
-  return <img src={imgSource} alt={app.name} />;
-};
-
-const renderInfo = (title, uri) => {
-  return uri ? (
-    <p className="info">
-      <a href={uri} target="_blank">
-        {title}
-      </a>
-    </p>
-  ) : null;
-};
-
 const renderLinks = app => {
   if (!app.linkAppStore && !app.linkPlayStore) {
-    return;
+    return <p />;
   }
 
   const linkAppStore = app.linkAppStore ? (
@@ -59,56 +62,140 @@ const renderLinks = app => {
   ) : null;
 
   return (
-    <p>
+    <p className="showcaseLinks">
       {linkPlayStore}
-      {linkPlayStore && linkAppStore ? ' • ' : ''}
+      {linkPlayStore && linkAppStore ? <span> • </span> : ''}
       {linkAppStore}
     </p>
   );
 };
 
+const randomizeApps = apps =>
+  apps.filter(app => !app.group).sort(() => 0.5 - Math.random());
+
 const Showcase = () => {
   const {siteConfig} = useDocusaurusContext();
 
-  const showcaseApps = siteConfig.customFields.users;
-  const pinnedApps = showcaseApps.filter(app => app.pinned);
-  const featuredApps = showcaseApps
-    .filter(app => !app.pinned)
-    .sort((a, b) => a.name.localeCompare(b.name));
-  const apps = pinnedApps.concat(featuredApps);
+  const {meta, microsoft, shopify, others} = siteConfig.customFields.users;
+  const [pinnedRandomizedApps, setPinnedRandomizedApps] = useState([]);
+  const [randomizedApps, setRandomizedApps] = useState([]);
+
+  useEffect(() => {
+    setRandomizedApps(randomizeApps(others.filter(app => !app.pinned)));
+    setPinnedRandomizedApps(randomizeApps(others.filter(app => app.pinned)));
+  }, []);
 
   return (
     <Layout
-      title="Who's using React Native?"
+      title="Showcase"
       description="Thousands of apps are using React Native, check out these apps!">
-      <div className="showcaseSection">
-        <div className="prose">
-          <h1>Who's using React Native?</h1>
+      <Section background="dark">
+        <div className="sectionContainer headerContainer">
+          <h1>
+            Who is using <span>React Native</span>?
+          </h1>
           <p>
             Thousands of apps are using React Native, from established Fortune
-            500 companies to hot new startups. If you're curious to see what can
-            be accomplished with React Native, check out these apps!
+            500 companies to hot new startups. If you are curious to see what
+            can be accomplished with React Native, check out these apps!
           </p>
         </div>
-        <div className="logos">{apps.map(renderApp)}</div>
-        <a
-          className="form-button"
-          href="https://forms.gle/BdNf3v5hemV9D5c86"
-          target="_blank">
-          Fill out this form to apply to the customer spotlight.
-        </a>
-        <p>
-          A curated list of{' '}
-          <span>
+      </Section>
+      <Section>
+        <div className="showcaseSection">
+          <h2 className="withLogo">
+            <ThemedImage
+              alt="Meta logo"
+              width={140}
+              sources={{
+                light: useBaseUrl('/img/meta_positive_primary.svg'),
+                dark: useBaseUrl('/img/meta_negative_primary.svg'),
+              }}
+            />
+          </h2>
+          <p className="showcaseSectionDescription">
+            React Native is shaping mobile, web, and desktop experiences within
+            Meta’s product ecosystem, from Facebook Marketplace, Messenger
+            Desktop, Ads Manager to the Oculus companion app and many more.
+          </p>
+          <div className="logos">{meta.map(renderApp)}</div>
+        </div>
+        <div className="showcaseSection">
+          <h2 className="withLogo">
+            <ThemedImage
+              alt="Microsoft logo"
+              width={180}
+              sources={{
+                light: useBaseUrl('/img/microsoft-logo-gray.png'),
+                dark: useBaseUrl('/img/microsoft-logo-white.png'),
+              }}
+            />
+          </h2>
+          <p className="showcaseSectionDescription">
+            Microsoft leverages the power of React Native to deliver excellent
+            customer experiences in some of its most well known apps.
+            <br />
+            Microsoft doesn't stop at mobile platforms either -- Microsoft
+            leverages React Native to target desktop too! Find out more in the{' '}
+            <a
+              href="https://microsoft.github.io/react-native-windows/resources-showcase"
+              target="_blank">
+              dedicated showcase
+            </a>{' '}
+            for React Native Windows and macOS.
+          </p>
+          <div className="logos">{microsoft.map(renderApp)}</div>
+        </div>
+        <div className="showcaseSection">
+          <h2 className="withLogo">
+            <ThemedImage
+              alt="Shopify logo"
+              width={160}
+              sources={{
+                light: useBaseUrl('/img/shopify_logo_whitebg.svg'),
+                dark: useBaseUrl('/img/shopify_logo_darkbg.svg'),
+              }}
+            />
+          </h2>
+          <p className="showcaseSectionDescription">
+            All new mobile apps at Shopify are React Native and we are actively
+            migrating our flagship merchant admin app Shopify Mobile to React
+            Native as well. You can read more about React Native development at
+            Shopify on our{' '}
+            <a href="https://shopify.engineering/topics/mobile" target="_blank">
+              blog
+            </a>
+            .
+          </p>
+          <div className="logos">{shopify.map(renderApp)}</div>
+        </div>
+        <div className="showcaseSection showcaseCustomers">
+          <h2>Users Showcase</h2>
+          <div className="logos">
+            {pinnedRandomizedApps.map(renderApp)}
+            {randomizedApps.map(renderApp)}
+          </div>
+        </div>
+      </Section>
+      <Section background="dark">
+        <div className="sectionContainer footerContainer">
+          <a
+            className="formButton"
+            href="https://forms.gle/BdNf3v5hemV9D5c86"
+            target="_blank">
+            Apply to the Showcase by filling out this form
+          </a>
+          <p>
+            A curated list of{' '}
             <a
               key="demo-apps"
               href="https://github.com/ReactNativeNews/React-Native-Apps">
               open source React Native apps
-            </a>
-          </span>{' '}
-          is also being kept by <a href="https://infinite.red">Infinite Red</a>.
-        </p>
-      </div>
+            </a>{' '}
+            is maintained by <a href="https://infinite.red">Infinite Red</a>.
+          </p>
+        </div>
+      </Section>
     </Layout>
   );
 };
