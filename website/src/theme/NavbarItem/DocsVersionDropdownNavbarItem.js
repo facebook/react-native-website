2cad18d355c5a6b72706c8f9a1e9c779389494c3
Tim Yung <yungsters@fb.com>
2022-09-29 23:17:57 -0700

    Apply updated copyright to `*.js` files

--- website/src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
+++ website/src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
@@ -1,9 +1,10 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import React from 'react';
 import DefaultNavbarItem from '@theme/NavbarItem/DefaultNavbarItem';
 import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
