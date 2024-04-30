31db6e9c89a43f588d0b4085aa04192ec1d0356a
Jon Samp <sampjon@gmail.com>
2024-04-30 10:47:20 -0500

    adds hero section

--- website/src/pages/Home/Hero/index.js
+++ website/src/pages/Home/Hero/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
+import GridBackground from './GridBackground';
+import FloorBackground from './FloorBackground';
+import Logo from './Logo';
+import Devices from './Devices';
+import styles from './styles.module.css';
+
 export function Hero() {
   return (
-    <div>
-      <h1>Hero</h1>
-      <img src="/img/header_logo.svg" alt="logo" />
+    <div className={styles.container}>
+      <div className={styles.backgroundContainer}>
+        <div className={styles.gridBackground}>
+          <GridBackground />
+        </div>
+        <div className={styles.devices}>
+          <Devices />
+        </div>
+        <div className={styles.floorBackground}>
+          <FloorBackground />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <Logo />
+        <h1 className={styles.title}>React Native</h1>
+        <h2 className={styles.subtitle}>Learn once, write anywhere.</h2>
+        <div className={styles.buttonContainer}>
+          <a href="/docs/environment-setup" className={styles.primaryButton}>
+            Get Started
+          </a>
+          <a href="/docs/getting-started" className={styles.secondaryButton}>
+            Learn the Basics
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
