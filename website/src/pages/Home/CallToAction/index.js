4caa0fcba4d4056600da52e84b55136b207d61c4
Jon Samp <sampjon@gmail.com>
2024-04-30 23:41:07 -0500

    adds (c)

--- website/src/pages/Home/CallToAction/index.js
+++ website/src/pages/Home/CallToAction/index.js
@@ -1,19 +1,20 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
 
 import Logo from '../Logo';
 
-import BackgroundPattern from './BackgroundPattern';
 import styles from './styles.module.css';
 
 function CallToAction() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.background}>
-        <img
-          src="/img/homepage/cta-bg-pattern.png"
-          className={styles.backgroundImage}
-        />
-      </div>
+      <div className={styles.background} />
       <div className={styles.container}>
         <Logo />
         <h1 className={styles.title}>Welcome to the React Native community</h1>
