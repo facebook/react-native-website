3fc34edb803361567b13b8d6b9e4810caac1e1bb
Jon Samp <sampjon@gmail.com>
2024-04-30 22:13:50 -0500

    adds native section

--- website/src/pages/Home/Native/index.js
+++ website/src/pages/Home/Native/index.js
@@ -3,12 +3,28 @@ import React from 'react';
 import Section from '../components/Section';
 import SectionTitle from '../components/SectionTitle';
 
+import FlyOutIllustration from './FlyOutIllustration';
+import styles from './styles.module.css';
+
 function Native() {
   return (
     <Section>
       <SectionTitle
         title="Native development for everyone"
-        description="React Native lets you create truly native apps and doesn't compromise your users' experiences. It provides a core set of platform agnostic native components like View, Text, and Image that map directly to the platform’s native UI building blocks."
+        description={
+          <>
+            React Native lets you create truly native apps and doesn't
+            compromise your users' experiences. It provides a core set of
+            platform agnostic native components like <code>View</code>,{' '}
+            <code>Text</code>, and <code>Image</code> that map directly to the
+            platform's native UI building blocks.
+          </>
+        }
+      />
+      <img
+        src="/img/homepage/FlyoutIllustration.png"
+        alt="A React Native UI pointing out native elements like Views, ScrollViews, and more"
+        className={styles.flyoutIllustration}
       />
     </Section>
   );
