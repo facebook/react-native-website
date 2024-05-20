4f1bd546dffa1a3f15447710f2a478b68f6738bb
Jon Samp <sampjon@gmail.com>
2024-05-20 16:58:20 -0500

    Adds dark theme to "continue with expo" button

--- website/src/theme/BoxLink/index.js
+++ website/src/theme/BoxLink/index.js
@@ -19,10 +19,10 @@ export default function BoxLink({href, children}) {
             height="20"
             viewBox="0 0 20 20"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.rightArrowIcon}>
             <path
               d="M4.16699 9.99984H15.8337M15.8337 9.99984L10.0003 4.1665M15.8337 9.99984L10.0003 15.8332"
-              stroke="var(--ifm-color-gray-800)"
               stroke-width="1.66667"
               stroke-linecap="round"
               stroke-linejoin="round"
