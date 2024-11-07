0a5052c1312c43ec84c78778762f816b82a705e1
Vincent Anjiri <vincentanjiri12@gmail.com>
2024-11-07 19:16:38 +0300

    Refactor Section component to use concise arrow function syntax
    - Simplified the component by using an arrow function and implicit return.
    - Kept the structure and styles intact while improving code readability.

--- website/src/pages/Home/components/Section/index.js
+++ website/src/pages/Home/components/Section/index.js
@@ -6,15 +6,13 @@
  */
 
 import React from 'react';
-
 import styles from './styles.module.css';
 
-function Section({children}) {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>{children}</div>
-    </div>
-  );
-}
+const Section = ({ children }) => (
+  <div className={styles.wrapper}>
+    <div className={styles.container}>{children}</div>
+  </div>
+);
 
 export default Section;
+
