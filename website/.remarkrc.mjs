9b402cedaf91aed665b723baa955798ca5a968fd
Hichem Fantar <fantar.hichem@gmail.com>
2024-12-07 02:28:43 +0100

    Update remark configuration to include broken external links plugin and enhance URL handling

--- website/.remarkrc.mjs
+++ website/.remarkrc.mjs
@@ -1,8 +1,9 @@
+import packageJson from "./package.json" assert { type: "json" };
+
 export default {
   plugins: [
     [
-      // "@react-native-website/remark-lint-no-broken-external-links",
-      "remark-lint-no-dead-urls",
+      "@react-native-website/remark-lint-no-broken-external-links",
       {
         skipUrlPatterns: [
           // False positive, flagged as a bot and rate limited
@@ -10,8 +11,10 @@ export default {
           // TODO: replace the 2048 example repository with another suitable project
           "github.com/JoelMarcey",
         ],
-        from: "https://reactnative.dev/docs",
-        skipLocalhost: true,
+        baseUrl: "https://reactnative.dev/docs",
+        headers: {
+          "user-agent": `${packageJson.name}/${packageJson.version}`,
+        },
       },
     ],
   ],
