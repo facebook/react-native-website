ff2426fb36a61fdbbfd03b54c2080077766d7157
sunnylqm <sunnylqm@gmail.com>
2025-08-07 10:33:00 +0800

    Remove ExternalLink icon component and update styles with licensing information

---
+++ cnwebsite/src/theme/Icon/TV/index.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+export default function TV() {
+  return (
+    <svg
+      width="18"
+      height="19"
+      viewBox="0 0 18 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_367_389)">
+        <path
+          d="M12.75 2.75L9 5.75L5.25 2.75M5.1 16.25H12.9C14.1601 16.25 14.7902 16.25 15.2715 16.0048C15.6948 15.789 16.039 15.4448 16.2548 15.0215C16.5 14.5402 16.5 13.9101 16.5 12.65V9.35C16.5 8.08988 16.5 7.45982 16.2548 6.97852C16.039 6.55516 15.6948 6.21095 15.2715 5.99524C14.7902 5.75 14.1601 5.75 12.9 5.75H5.1C3.83988 5.75 3.20982 5.75 2.72852 5.99524C2.30516 6.21095 1.96095 6.55516 1.74524 6.97852C1.5 7.45982 1.5 8.08988 1.5 9.35V12.65C1.5 13.9101 1.5 14.5402 1.74524 15.0215C1.96095 15.4448 2.30516 15.789 2.72852 16.0048C3.20982 16.25 3.83988 16.25 5.1 16.25Z"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_367_389">
+          <rect
+            width="18"
+            height="18"
+            fill="white"
+            transform="translate(0 0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
---
+++ cnwebsite/src/theme/Icon/Web/index.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+export default function Web() {
+  return (
+    <svg
+      width="18"
+      height="19"
+      viewBox="0 0 18 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_367_381)">
+        <path
+          d="M9 2C10.876 4.05376 11.9421 6.71903 12 9.5C11.9421 12.281 10.876 14.9462 9 17M9 2C7.12404 4.05376 6.05794 6.71903 6 9.5C6.05794 12.281 7.12404 14.9462 9 17M9 2C4.85786 2 1.5 5.35786 1.5 9.5C1.5 13.6421 4.85786 17 9 17M9 2C13.1421 2 16.5 5.35786 16.5 9.5C16.5 13.6421 13.1421 17 9 17M1.87501 7.25H16.125M1.875 11.75H16.125"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_367_381">
+          <rect
+            width="18"
+            height="18"
+            fill="white"
+            transform="translate(0 0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
