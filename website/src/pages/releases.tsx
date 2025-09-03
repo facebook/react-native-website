a907390d651598c63d4eec098b2799465bcce103
Simek <gosimek@gmail.com>
2025-09-03 11:15:43 +0200

    client side redirect attempt

--- website/src/pages/releases.tsx
+++ website/src/pages/releases.tsx
@@ -4,9 +4,19 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import Layout from '@theme/Layout';
+import React, {useEffect} from 'react';
 
 const Releases = () => {
-  location.href = '/docs/next/releases';
+  useEffect(() => {
+    location.href = '/docs/next/releases';
+  }, []);
+
+  return (
+    <Layout title="Releases" wrapperClassName="versions-page">
+      <div style={{textAlign: 'center'}}>Redirecting...</div>
+    </Layout>
+  );
 };
 
 export default Releases;
