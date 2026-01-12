09c93053b8ad745fd895556b5b37a02d61a5553b
Simek <gosimek@gmail.com>
2026-01-12 23:12:57 +0100

    sanitize tape input/output, run plugins checks on CI

--- plugins/remark-snackplayer/tests/index.ts
+++ plugins/remark-snackplayer/tests/index.ts
@@ -17,18 +17,30 @@ function read(name: string) {
   return fs.readFileSync(path.join(import.meta.dirname, name), 'utf8');
 }
 
+function cleanupStringContent(content: string) {
+  return content.replaceAll('\r\n', '\n').trim().replace(/\s+/g, '');
+}
+
 test('remark-snackplayer', async t => {
   const processor = remark().use(remarkMdx).use(SnackPlayer);
 
   const in1 = read('markdown/test1.md');
   const out1 = read('output/output1.html');
   const file1 = await processor.process(in1);
-  t.equal(String(file1), out1, 'With 1 Code Block');
+  t.equal(
+    cleanupStringContent(String(file1)),
+    cleanupStringContent(out1),
+    'With 1 Code Block'
+  );
 
   const in2 = read('markdown/test2.md');
   const out2 = read('output/output2.html');
   const file2 = await processor.process(in2);
-  t.equal(String(file2), out2, 'With 2 Code Blocks');
+  t.equal(
+    cleanupStringContent(String(file2)),
+    cleanupStringContent(out2),
+    'With 2 Code Blocks'
+  );
 
   t.end();
 });
